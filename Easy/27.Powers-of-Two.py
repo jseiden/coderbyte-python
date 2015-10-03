a339ee71d30608c7d4c8bdee89e71f0f4d5ab874
jseiden <jseiden@gmail.com>
2015-10-02 23:22:06 -0700

    Powers-of-Two

---
+++ Easy/27.Powers-of-Two.py
@@ -0,0 +1,13 @@
+# Using the Python language, have the function PowersofTwo(num) take the num parameter being passed 
+# which will be an integer and return the string true if it's a power of two. 
+# If it's not return the string false. For example if the input is 16 then your program should return the string true 
+# but if the input is 22 then the output should be the string false. 
+
+def PowersOfTwo(num):
+    while num > 1:
+        if num == 2.0:
+            return "true"
+        else:
+            num = float(num)/float(2)
+    return "false"
+
